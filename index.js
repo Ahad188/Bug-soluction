fb2d34f39eaa181071d72fa017e54fa099b0dd1e
AhaD <abuahad35188@gmail.com>
2023-03-15 23:51:42 +0600

    show Question done

--- index.js
+++ index.js
@@ -46,8 +46,9 @@ startQuiz.addEventListener("click", () => {
 // All quiz data fetched from json
 const loadQuiz = async () => {
   const res = await fetch("./data/quiz.json");
-  const data = await res.json;
-  quizData = data;
+  const data = await res.json();
+//   quizData = data;
+// console.log(data)
   displayQuiz(data);
 };
 
@@ -57,14 +58,15 @@ const displayQuiz = (data) => {
     quizContainer.innerHTML = "";
     return;
   }
-
-  data.forEach((quiz, i) => {
+  
+  data.map((quiz, i) => {
+     console.log(quiz)
     quizContainer.innerHTML += `<div class="m-3 py-3 px-4 shadow-sm rounded">
   <div class="flex items-center">
     <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
       ${i + 1}
     </div>
-    <p class="text-gray-800 text-sm">${quiz.quetion}</p>
+    <p class="text-gray-800 text-sm">${quiz.question}</p>
   </div>
   <div class="grid grid-cols-2 gap-4 mt-5">
     ${displayQuizOptions(quiz.options, i)}
@@ -79,7 +81,7 @@ document.querySelector("#submit").addEventlistener("click", () => {
     return;
   }
   quizTimer(true);
-  answersContainer.innerHTML = `<div class="my-4">
+  answersContainer.innerHTML += `<div class="my-4">
   <i class="fa-solid fa-fan animate-spin text-2xl text-green-600"></i>
   <p class="text-xs animate-pulse">Please Wait, We are checking...</p>
 </div>`;
